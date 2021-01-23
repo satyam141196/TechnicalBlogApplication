62fe53a9f856930139ee51bd2ecbf0898e9f338b
satyam141196 <satyam141196@gmail.com>
2021-01-23 20:18:17 +0530

    First Commit

---
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -0,0 +1,17 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import technicalblog.controller.HomeController;
+
+@SpringBootApplication
+@ComponentScan
+public class TechnicalBlogApplication {
+
+    public static void main(String [] args){
+        SpringApplication.run(TechnicalBlogApplication.class, args);
+    }
+
+}
